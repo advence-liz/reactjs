c6849173df26ec135d14be8745a210d25fcb01a0
advence-liz <1012515183@qq.com>
2017-07-31 20:28:40 +0800

    计划add router 写一些自己写小demo

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # reactjs
 for react 
-
+## REF
+- [react-router](https://reacttraining.com/react-router/web/example/basic)
 ## AMD
 创建一个branch 存储之前的AMD 打包方案
 -
@@ -13,3 +14,7 @@ $ npm install reactjs_study
 ## tip
 - 我发现 npm publish 会自动将项目中的 .gitignore 转为 .npmignore
 - page.json 中的 dependencs 会被自动下载到 node_modules,而 devDependences不会下载
+- page.json 配置bin 字段然后全局安装就是开发命令行工具了
+
+## TODO
+-[ ] 做一个命令行工具可以可以自动编译less 压缩 css 压缩 js 运行 webpack 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import a from "./a"
-console.log(a);
-//console.log(d);
-//fun();
-ReactDOM.render(
-        <h1>Hello, world!</h1>,
-        document.getElementById('root')
-      );
+import { Router, Route, hashHistory } from 'react-router';
+function App(props) {
+  return <h1>Hello,App</h1>;
+}
+function Repos(props) {
+  return <h1>Hello, Repos</h1>;
+}
+function About(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
 
+ReactDOM.render((
+  <Router history={hashHistory}>
+  <Route path="/" component={App}/>
+  <Route path="/repos" component={Repos}/>
+  <Route path="/about" component={About} name="About"/>
+  </Router>
+), document.getElementById('root'));
