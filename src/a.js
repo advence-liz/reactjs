1318d77efdd7a12e633d48b927a2f93f905424da
advence-liz <1012515183@qq.com>
2017-08-01 17:13:23 +0800

    upadte

--- src/a.js
+++
@@ -1,8 +0,0 @@
-function liz() {
-    console.log("eeee");
-}
-
-var a = { liz: "eeee" };
-
-export default {a:a,fun:liz};
-
--- src/index.js
+++ src/index.js
@@ -1,20 +1,93 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
-function App(props) {
-  return <h1>Hello,App</h1>;
+import ReactDOM, { render } from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <iframe src="https://gist.github.com/advence-liz"/>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About!!!!org</h2>
+  </div>
+)
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic} />
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )} />
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+/**
+ * construct of indexItem
+ * @constructor
+ * @prop {String} title link title
+ * @prop {String} path  link path
+ */
+class IndexItem {
+  constructor(title, path) {
+    this.title = title;
+    this.path = path;
+  }
 }
-function Repos(props) {
-  return <h1>Hello, Repos</h1>;
-}
-function About(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
+// let componentList=[]
+let indexList = [new IndexItem('Home',`/`),new IndexItem('About',`/about`),new IndexItem('Topics',`/topics`)];
+const linkList = indexList.map(function(item){
+ return <li><Link to={item.path} >{item.title}</Link></li>
+})
+const routeList=indexList.map((item)=> <Route exact path={item.path} component={Home} /> )
+const Index = () => (
+  <Router>
+    <div>
+      <ul>
+       {linkList}
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+    </div>
+  </Router>
+)
+
+
+
 
 ReactDOM.render((
-  <Router history={hashHistory}>
-  <Route path="/" component={App}/>
-  <Route path="/repos" component={Repos}/>
-  <Route path="/about" component={About} name="About"/>
-  </Router>
+  <Index />
 ), document.getElementById('root'));
