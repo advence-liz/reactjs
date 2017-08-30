36ceac694fe5c36e7e8d713d40cf51b25ccbfa51
advence-liz <1012515183@qq.com>
2017-08-30 17:22:22 +0800

    update

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM, { render } from 'react-dom';
+
+render(
+    <h1>hellow world</h1>,
+    document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,25 @@
 var webpack = require('webpack');
 var path = require("path"),
     HtmlWebpackPlugin = require('html-webpack-plugin');
-//  CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
 
-console.log(path.join(__dirname,"..",".."));
+
+
 module.exports = {
     entry: {
-        bundle: "./src/index.js",
+        bundle: "./src/index.jsx",
 
-        //   vendor: ["react", "react-dom"]
+      
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "[name].js",
-        // library: "[name]_[hash]"
-        // chunkFilename: '[name]-[id].js',
-        // library: "$d",
-        // publicPath: "../build/"
+      
 
     },
 
     module: {
         rules: [
-            // {
-            //     test: /\.jsx$/,
-            //     enforce: "pre",
-            //     loader: "eslint-loader"
-            // },
+            
             {
                 test: /\.(js|jsx)$/,
                 exclude: /(node_modules|bower_components)/,
@@ -62,42 +55,8 @@ module.exports = {
             filename: 'index.html',
             template: 'template/_layout.html'
         }),
-
-        // new webpack.DllReferencePlugin({
-        //     context:path.resolve(__dirname,".."),
-        //     manifest: require('./js/vendor-manifest.json'),
-        // })
-        /**
-         *引入vendor 引入第三方库（node_modules)的时候，不依赖 CommonsChunkPlugin插件也有效自己定义的文件入口不行
-         */
-        // new webpack.optimize.CommonsChunkPlugin({
-        //     name: "vendor",
-
-        //     // filename: "vendor.js"
-        //     // (Give the chunk a different name)
-
-        //     minChunks: Infinity,
-        //     // (with more entries, this ensures that no other module
-        //     //  goes into the vendor chunk)
-        // })
-//         window.__lib.copy=require("copy-to-clipboard"); 
-// window.__lib.CryptoJS=require("crypto-js");
-// window.__lib["prop-types"]=require("prop-types"); 
-// window.__lib["babel-polyfill"]=require("babel-polyfill");
-      
     ],
-    externals: {
-        'react': ' window.__lib.React',
-        'react-dom': ' window.__lib.ReactDOM',
-        'react-router-dom': ' window.__lib.ReactRouterDOM',
-        'redux': ' window.__lib.Redux',
-        'react-redux': ' window.__lib.ReactRedux',
-        'react-router': ' window.__lib.ReactRouter',
-        'crypto-js':'window.__lib.CryptoJS',
-        'copy-to-clipboard':'window.__lib.Copy',
-        'babel-polyfill':'window.__lib.BabelPolyfill',
-        'prop-types':'window.__lib.PropTypes'
-    },
+    
     devServer: {
         contentBase: path.join(__dirname, "dist"),
         compress: true,
