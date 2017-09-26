679a21c5d345660ffbfa18c4abe0c9f75ca05f17
advence-liz <1012515183@qq.com>
2017-09-26 15:12:31 +0800

    update 之后开分支

--- launch.cmd
+++ launch.cmd
@@ -1,5 +1,11 @@
-
+@echo off
+color 2
 start cmd /c npm run start-server
 ping -n 3 127.0.0.1
 start http://localhost:9000/
 exit
+
+
+REM echo start webpack-dev-server
+REM title webpack-dev-server
+REM node ..\..\node_modules\webpack-dev-server\bin\webpack-dev-server --inline --hot
--- soureces/router/launch.cmd
+++ soureces/router/launch.cmd
@@ -3,4 +3,4 @@
 color 2
 echo start webpack-dev-server
 title webpack-dev-server
-node ../../node_modules\webpack-dev-server\bin\webpack-dev-server --inline --hot
+node ..\..\node_modules\webpack-dev-server\bin\webpack-dev-server --inline --hot
