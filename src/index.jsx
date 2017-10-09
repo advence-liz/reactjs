49948160a8319f85645d77cf1f13369252659306
advence-liz <1012515183@qq.com>
2017-10-09 14:33:15 +0800

    CodeBlock

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-// require("prop-types");
-//require("babel-polyfill");
 import ReactDOM, { render } from 'react-dom';
-import version,{log} from './log.js';
-log();
-console.log(version);
-render((
-  <h1>Hello, world!!</h1>
-), document.getElementById('root'));
+import Remarkable from 'remarkable';
+import Markdown from 'react-remarkable';
+const remarkable = new Remarkable();
 
-ReactDOM.render((
-  <h1>Hello, world!!</h1>
-), document.getElementById('root'));
 
-export { log};
+
+class CodeBlock extends React.Component{
+  constructor(props){
+    super(props);
+  }
+}
