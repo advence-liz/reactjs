69b5d0a045e596032adee3423c5ac0fbb62eaafe
advence-liz <1012515183@qq.com>
2017-11-17 17:08:59 +0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
-import version,{log} from './log.js';
-log();
-console.log(version);
-render((
-  <h1>Hello, world!!</h1>
-), document.getElementById('root'));
 
-ReactDOM.render((
-  <h1>Hello, world!!</h1>
-), document.getElementById('root'));
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+      title: this.props.title,
+      time: 0
+    };
+  }
 
-export { log};
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+    this.setState((prevState, props) => ({
+      title: `${prevState.time++}${props.title}`
+    }));
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.time % 2) {
+      return false;
+    }
+    return true;
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(
+  <Clock title="Hello" />,
+  document.getElementById('root')
+);
