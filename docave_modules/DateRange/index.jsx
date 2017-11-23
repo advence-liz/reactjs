9392a8f7093a93802ed10327690fc5735b777a86
advence-liz <1012515183@qq.com>
2017-11-23 17:32:11 +0800

    update

--- docave_modules/DateRange/index.jsx
+++ docave_modules/DateRange/index.jsx
@@ -1,19 +1,5 @@
-// public enum JobDateRangeType
-// {
-//     [Description("All Jobs")]
-//     AllJobs = 0,
-//     [Description("Last 7 Days")]
-//     Last7Days = 1,
-//     [Description("Last 14 Days")]
-//     Last14Days = 2,
-//     [Description("Last 30 Days")]
-//     Last30Days = 3,
-//     [Description("Customize")]
-//     Customize = 4,
-//     [Description("Last 24 Hours")]
-//     Last24Hours = 5,
-// }
-const DateRangeSources = [{
+
+const DateRangeTypes = [{
     name: 'All Jobs',
     value: 0
 }, {
@@ -33,43 +19,73 @@ const DateRangeSources = [{
     name: 'Customize',
     value: 4
 }];
-
+const ScheduleDateRangeTypes = [{
+    name: 'Next 7 Days',
+    value: 0
+}, {
+    name: 'Next 14 Days',
+    value: 1
+}, {
+    name: 'Next 30 Days',
+    value: 2
+}, {
+    name: 'Customize',
+    value: 3
+}];
+/**
+ * 
+ * @param {Date} date Date 浏览器端Date对象
+ * @param {Number} targetOffset 目标偏移量 -12 <= targetOffset <= +12
+ */
+function convertDate2Ticks(date, targetOffset) {
+    let currentTimeZone = -date.getTimezoneOffset() / 60;
+    let times, ticks
+    times = date.getTime() + (currentTimeZone + targetOffset) * 60 * 60 * 1000;
+    ticks = times * 10000 + 621355968000000000;
+    return ticks;
+}
+// <DateRange schedule />
 export default class DateRange extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedTimeZone: $$.I18N.timezones[1],
+            selectedTimeZone: $$.I18N.timezones[0],
             disabled: true
         }
-        this.DateRangeCustomize = false;
-        this.RangeType = null;
-        
+        this.status = {
+            FromTime: 0,
+            ToTime: 0,
+            RangeType: 0
+        };
+
 
         this.selectionChanged = this.selectionChanged.bind(this);
         this.dateChanged = this.dateChanged.bind(this);
-        this.submit = this.submit.bind(this);
-        
-
+        this.cancel = this.cancel.bind(this);
     }
     selectionChanged(e, args) {
         let current = args.newValue.item;
         if (current.name === "Customize") {
             this.setState({ disabled: false });
-            this.DateRangeCustomize=true;
+            // this.status.DateRangeCustomize = true;
         } else {
             this.setState({ disabled: true });
-            this.DateRangeCustomize=false;
+            // this.status.DateRangeCustomize = false;
         }
-        this.RangeType = current.value;
+        this.status.RangeType = current.value;
     }
     dateChanged(e, args) {
-        console.table(args);
-    }
-    submit(){
+        let item = args.newValue;
+        let targetOffset = parseInt(item.timezone.baseUtcOffset.slice(0, 3));
+        this.status.FromTime = convertDate2Ticks(item.start, targetOffset);
+        this.status.ToTime = convertDate2Ticks(item.end, targetOffset);
 
     }
-    cancel(){
 
+    cancel() {
+        this.setState({ selectedTimeZone: $$.I18N.timezones[0] });
+        //close popupp
+        $(document.body).trigger("mousedown");
     }
     render() {
         let combobox = "data-range--combobox",
@@ -86,7 +102,8 @@ export default class DateRange extends React.Component {
             dateRangeBottomStyle = {
                 textAlign: 'right',
                 padding: 5
-            }
+            },
+            currentDateRangeTypes = this.props.schedule ? ScheduleDateRangeTypes : DateRangeTypes;
         return (
             <div className="date-range" style={dateRangeStyle}>
                 <div className="date-range--top" style={dateRangeTopStyle}>
@@ -95,9 +112,9 @@ export default class DateRange extends React.Component {
                         id={combobox}
                         dataTextField="name"
                         dataValueField="value"
-                        items={DateRangeSources}
+                        items={currentDateRangeTypes}
                         selectionChanged={this.selectionChanged}
-                        selectedItem={DateRangeSources[0]}
+                        selectedItem={currentDateRangeTypes[0]}
                     />
                     <label htmlFor={rangePicker}>job(s) start during:</label>
                     <R.Rangepicker
@@ -109,7 +126,7 @@ export default class DateRange extends React.Component {
                     />
                 </div>
                 <div className="date-range--bottom" style={dateRangeBottomStyle}>
-                    <button type="button" className="button button-default" onClick={this.submit}>OK</button>
+                    <button type="button" className="button button-default" onClick={this.props.submit.bind(null, this.status)}>OK</button>
                     <button type="button" className="button button-link button-blue button-underline" onClick={this.cancel}>Cancel</button>
                 </div>
             </div>
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 //import Datagrid from 'Datagrid';
-import DateRange from 'DateRange'
+import DateRange from 'DateRange';
+$$.I18N.timezones=[{"id":"Dateline Standard Time","displayName":"(UTC-12:00) International Date Line West","zone":"(UTC-12:00)","baseUtcOffset":"-12:00:00","supportsDaylightSavingTime":false,"hashCode":"-28726934","kind":0,"adjustmentRules":[]},{"id":"UTC-11","displayName":"(UTC-11:00) Coordinated Universal Time-11","zone":"(UTC-11:00)","baseUtcOffset":"-11:00:00","supportsDaylightSavingTime":false,"hashCode":"524007363","kind":0,"adjustmentRules":[]},{"id":"Samoa Standard Time","displayName":"(UTC-11:00) Samoa","zone":"(UTC-11:00)","baseUtcOffset":"-11:00:00","supportsDaylightSavingTime":false,"hashCode":"-758128916","kind":0}];
+class Root extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.submitDateRange = this.submitDateRange.bind(this);
+  }
+  submitDateRange(status) {
+    debugger;
+  }
+  render() {
+    return (<DateRange  submit={this.submitDateRange} />)
+  }
+}
 ReactDOM.render((
-  <DateRange />
+  <Root />
 ), document.getElementById('root'));
+
