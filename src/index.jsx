66992a66fe697f53cb3d430e2fa9afe62dff038b
advence-liz <1012515183@qq.com>
2018-01-10 15:54:02 +0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
-import {GrantPermissions} from 'Permissions';
+import { GrantPermissions } from 'Permissions';
 require('Mock');
 import _ from "lodash";
-import Enum ,{CAOperation} from "./Operation";
-let status ={
-  liz:0,
-  cc:1,
-  dd:2
+import Enum, { CAOperation } from "./Operation";
+let status = {
+  liz: 0,
+  cc: 1,
+  dd: 2
 }
-let operation ={siteCollectionUrl:`https://dddd.cdfcdf`}
+let operation = { siteCollectionUrl: `https://dddd.cdfcdf` }
 CAOperation.getName(operation);
 console.log(CAOperation.getName(operation))
 console.log(CAOperation.getID(operation))
-var key= Enum.getName(status,0);
+var key = Enum.getName(status, 0);
 console.log(key);
 class Root extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-     
+
     };
 
     this.submitDateRange = this.submitDateRange.bind(this);
   }
   submitDateRange(status) {
-   
+
   }
   render() {
-   
-  
+
+
     return (<GrantPermissions submit={this.submitDateRange} />)
   }
 }
