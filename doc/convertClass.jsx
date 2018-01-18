5ed28c90453f4ab96e08805553138745dc1b4406
advence-liz <1012515183@qq.com>
2018-01-18 08:52:15 +0800

    update

---
+++ doc/convertClass.jsx
@@ -0,0 +1,53 @@
+ class CAListAnonymousAccessOperation : CAOperation
+{
+    
+      AddItemEnabled
+
+    
+      EditItemEnabled 
+
+    
+      DeleteItemEnabled 
+
+    
+      ViewItemEnabled 
+
+    
+      BreakInheritNodes 
+
+    
+     AnonymousPermission SelectedAnonymousPerms 
+}
+
+
+
+  AnonymousPermission 
+{
+    
+    EmptyMask = 0,
+    
+    ViewListItems = 1,
+    
+    AddListItems = 2,
+    
+    EditListItems = 4,
+    
+    DeleteListItems = 8,
+}
+
+
+  AnonymousEnableStatus 
+{
+    
+    AllDisable = 0,
+    
+    ViewItemEnable = 1,
+    
+    AddItemsEnable = 2,
+    
+    EditItemsEnable = 4,
+    
+    DeleteItemsEnable = 8,
+    
+    AllEnable = 15,
+}
