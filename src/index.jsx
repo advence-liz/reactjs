bc23c75e77f469ec302ccdada708edb104e46a92
advence-liz <1012515183@qq.com>
2018-05-26 12:17:45 +0800

    updatea:yarn

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 
 class Clock extends React.Component {
+  name ="eee";
   constructor(props) {
     super(props);
     this.state = {
